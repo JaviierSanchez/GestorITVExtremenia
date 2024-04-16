fdb87fe29950aab58f8f11cc9717f9285626d8de
Javier <javierscerrato@gmail.com>
2024-04-16 21:04:45 +0200

    Proyecto Final: Pantalla Pedir Cita

--- src/main/java/com/jorja/proyect/proyectogestoritvfinal/vista/VentanaPrincipal.java
+++ src/main/java/com/jorja/proyect/proyectogestoritvfinal/vista/VentanaPrincipal.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,7 +17,7 @@ public class VentanaPrincipal extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        VBox root = FXMLLoader.load(getClass().getResource(VENTANAPRINCIPAL));
+        BorderPane root = FXMLLoader.load(getClass().getResource(VENTANAPRINCIPAL));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
